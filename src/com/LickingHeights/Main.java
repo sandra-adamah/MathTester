2c4b48513ba16358f97d826f72e006986b195cb6
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-02-04 14:04:24 -0500

    Created intelij project

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package com.LickingHeights;
+
+public class MathTester {
+
+    public static void main(String[] args){
+        int firstNumber, secondNumber, thirdNumber;
+        firstNumber = 3;
+        secondNumber = 8;
+        thirdNumber = 4;
+
+        String firstWord;
+       // firstWord =
+
+    }
+}
