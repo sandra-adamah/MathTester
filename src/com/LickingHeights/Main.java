7033f6699226a26274524d08dea1f43b2c7bd030
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-02-04 14:10:32 -0500

    Created and initialized first and second word

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,13 +3,19 @@ package com.LickingHeights;
 public class MathTester {
 
     public static void main(String[] args){
+        String firstWord;
+        String secondWord;
+
+       firstWord = "dog";
+       secondWord = "house"
+
         int firstNumber, secondNumber, thirdNumber;
         firstNumber = 3;
         secondNumber = 8;
         thirdNumber = 4;
 
-        String firstWord;
-       // firstWord =
+
+       firstWord =
 
     }
 }
