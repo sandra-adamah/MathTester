e9fe9746ad3caf9a9ea1048a2c0c8c1ccfacb0e9
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-02-05 14:08:46 -0500

    comment blocked code and added total to int

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,19 +9,22 @@ public class Main {
        firstWord = "dog";
        secondWord = "house";
 
-        int firstNumber, secondNumber;
+        int firstNumber, secondNumber, total;
 
         firstNumber = 5;
         secondNumber = 10;
+        total = firstNumber+secondNumber;
+        System.out.println(total); 
 
-        System.out.println(firstWord+secondWord);
+
+        /*System.out.println(firstWord+secondWord);
         System.out.println(firstWord+(firstNumber+secondNumber));
         System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
         System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
         System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
         System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
         System.out.println("The modulus of the secondNumber by the firstNumber" +"is "+
-                (secondNumber%firstNumber));
+                (secondNumber%firstNumber));*/
 
     }
 }
